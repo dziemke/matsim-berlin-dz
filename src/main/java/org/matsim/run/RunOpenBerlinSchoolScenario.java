d78eb95a79fb02a5254c7152d664a55b249571a4
tschlenther <schlenther@vsp.tu-berlin.de>
2020-04-03 16:03:21 +0200

    set up for both 1pct and 10 pct

--- src/main/java/org/matsim/run/RunOpenBerlinSchoolScenario.java
+++ src/main/java/org/matsim/run/RunOpenBerlinSchoolScenario.java
@@ -25,13 +25,28 @@ import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
 
 class RunOpenBerlinSchoolScenario {
 
-    private static final String CONFIG_PATH = "../../svn/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.4-1pct-schools/input/berlin-v5.4-1pct-schools.config.xml";
+    private static final String CONFIG_PATH_1PCT = "../../svn/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.4-1pct-schools/input/berlin-v5.4-1pct-schools.config.xml";
+    private static final String OUTPUT_DIR_1PCT = "../../svn/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.4-1pct-schools/output-berlin-v5.4-1pct-schools/";
 
-    private static final String OUTPUT_DIR = "../../svn/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.4-1pct-schools/output-berlin-v5.4-1pct-schools/";
+    private static final String CONFIG_PATH_10PCT = "../../svn/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.4-10pct-schools/input/berlin-v5.4-10pct-schools.config.xml";
+    private static final String OUTPUT_DIR_10PCT = "../../svn/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.4-10pct-schools/output-berlin-v5.4-10pct-schools/";
 
     public static void main(String[] args) {
 
-        Config config = RunBerlinScenario.prepareConfig(new String[]{CONFIG_PATH});
+        boolean doOnePercent = false;
+
+
+
+        String configFile, outputDir;
+        if(doOnePercent){
+             configFile = CONFIG_PATH_1PCT;
+             outputDir = OUTPUT_DIR_1PCT;
+        } else {
+            configFile = CONFIG_PATH_10PCT;
+            outputDir = OUTPUT_DIR_10PCT;
+        }
+
+        Config config = RunBerlinScenario.prepareConfig(new String[]{configFile});
 
         //school attendees have normal "home" activities without any suffix..
         config.planCalcScore().addActivityParams((new PlanCalcScoreConfigGroup.ActivityParams("home")).setTypicalDuration((double) 8*3600));
@@ -41,7 +56,7 @@ class RunOpenBerlinSchoolScenario {
         config.planCalcScore().addActivityParams((new PlanCalcScoreConfigGroup.ActivityParams("educ_secondary")).setTypicalDuration((double) 8*3600));
 
 //        config.planCalcScore().setFractionOfIterationsToStartScoreMSA(0.8); //do not know why it is not set beforehand.. does not matter here anyways
-        config.controler().setOutputDirectory(OUTPUT_DIR);
+        config.controler().setOutputDirectory(outputDir);
         config.controler().setRunId("output-berlin-v5.4-1pct-schools");
         config.controler().setLastIteration(0);
 
